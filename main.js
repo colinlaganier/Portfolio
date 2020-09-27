3288aa8b205e8c264df68f5276c2188cc307887f
Colin Laganier <cul18@ic.ac.uk>
2020-09-27 14:57:10 +0100

    Satisfactory main page layout

--- main.js
+++ main.js
@@ -1,4 +1,11 @@
 $(document).ready(function() {
+    // $(".header-name").click(function(){
+        $(".stripes").animate({
+            height:'100%'
+        },
+        'slow');
+    // });
+
     $("#projects-button").click(function() {
         $('html,body').animate({
             scrollTop: $("#test").offset().top
