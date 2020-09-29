ae6882f448cd4b28fd9a9514ff901232ac2bbf0b
Colin Laganier <cul18@ic.ac.uk>
2020-09-29 12:45:52 +0100

    Modyfied stripe animation

--- public/Js/projects.js
+++ public/Js/projects.js
@@ -1,6 +1,14 @@
 $(document).ready(function() {
+    $(".stripes-title").animate({
+        height: $("#title-height").height()
+    },
+    'slow');
+});
+
+$(window).on('resize', function(){
+    if ( $("#title-height").height() != $(".stripes-title").height()){
         $(".stripes-title").animate({
-            height:'80px'
-        },
-        'slow');
+            height: $("#title-height").height()
+        },"slow");
+    }
 });
